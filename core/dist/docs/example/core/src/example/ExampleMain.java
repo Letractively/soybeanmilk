ca877a92aeb81b8910e0ce41d1fc665679dadf87
earthangry <earthangry@gmail.com>
2012-05-10 15:46:10 +0800

    为所有单元测试用例代码和示例代码添加许可和作者内容

--- core/dist/docs/example/core/src/example/ExampleMain.java
+++ core/dist/docs/example/core/src/example/ExampleMain.java
@@ -1,3 +1,17 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ */
+
 package example;
 
 import org.soybeanMilk.core.Constants;
@@ -8,6 +22,10 @@ import org.soybeanMilk.core.config.parser.ConfigurationParser;
 import org.soybeanMilk.core.os.ConvertableObjectSource;
 import org.soybeanMilk.core.os.HashMapObjectSource;
 
+/**
+ * @author earthangry@gmail.com
+ * @date 2012-5-10
+ */
 public class ExampleMain
 {
 	public static void main(String[] args) throws Exception
--- core/src/test/test/unit/core/TestGenericType.java
+++ core/src/test/test/unit/core/TestGenericType.java
@@ -1,5 +1,18 @@
-package test.unit.core;
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ */
 
+package test.unit.core;
 
 import java.lang.reflect.Method;
 import java.lang.reflect.Type;
@@ -14,6 +27,10 @@ import org.soybeanMilk.core.bean.GenericType;
 
 import test.unit.core.TestGenericType.C.CInner;
 
+/**
+ * @author earthangry@gmail.com
+ * @date 2012-5-10
+ */
 public class TestGenericType
 {
 	@Before
